d6fd2580ba390aa16e544b4f1feef64c4ce7c3f8
TyperGuy <ray.developper@gmail.com>
2022-08-10 21:10:50 +0100

    feat: use multiple reducers functions

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 
 function buyCake (){
   return{
     type : BUY_CAKE,
-    info:'Now We are Guying a Cake'
+    info:'Now We are Buying a Cake'
+  }
+}
+function buyIcecream (){
+  return{
+    type : BUY_ICECREAM,
+    info:'Now We are Buying a IceCream'
   }
 }
 
 const initialState = {
-  numOfCake : 10
+  numOfCake : 10,
+  numOfIceCream : 30
 }
 
 const reducer = (state = initialState, action)=>{
@@ -21,6 +29,11 @@ const reducer = (state = initialState, action)=>{
       return{
         ...state,
         numOfCake:state.numOfCake-1
+      }
+    case BUY_ICECREAM:
+      return{
+        ...state,
+        numOfIceCream:state.numOfIceCream-1
       }
     default: return state
   }
@@ -31,7 +44,12 @@ const store = createStore(reducer);
 console.log('Initial State :' ,store.getState());
 const unsubcribe = store.subscribe(()=> console.log('Updated State',store.getState()));
 store.dispatch(buyCake());
+store.dispatch(buyIcecream()); 
 store.dispatch(buyCake());
+store.dispatch(buyIcecream()); 
 store.dispatch(buyCake());
+store.dispatch(buyIcecream()); 
+store.dispatch(buyCake());
+store.dispatch(buyIcecream()); 
 
 unsubcribe();
